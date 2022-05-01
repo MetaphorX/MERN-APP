4abfac6abbf8fea7a35158bd50e9aaf0e50e0758
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-01 11:20:04 +0100

    facing type errors

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 const getGoals = asyncHandler(async (req, res) =>{
 
@@ -29,7 +30,18 @@ const updateGoal = asyncHandler(async (req, res) =>{
         res.status(400)
         throw new Error("Goal not found")
     }
+    const user = await User.findById(req.user.id)
 
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+
+    }
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, 
         {
             new:true,
@@ -45,6 +57,19 @@ const deleteGoal = asyncHandler(async (req, res) =>{
     if(!goal){
         res.status(400)
         throw new Error("Goal not found")
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('user not found')
+    }
+
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+
     }
 
     await goal.remove()
